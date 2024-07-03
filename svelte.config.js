bf75dfa93d41bda97825bd0c97db901c09bc75f6
Lycia Stenson <115705933+LyciaStenson@users.noreply.github.com>
2024-07-03 22:52:38 +0100

    Update

--- svelte.config.js
+++ svelte.config.js
@@ -12,11 +12,11 @@ const config = {
 
 	kit: {
 		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: undefined,
-			precompress: false,
-			strict: true
+			//pages: 'build',
+			//assets: 'build',
+			fallback: "404.html"
+			//precompress: false,
+			//strict: true
 		}),
 		paths: {
 			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
