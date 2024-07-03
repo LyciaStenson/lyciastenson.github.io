85c92831792c8fbfb12d533a74460e596794614c
Lycia Stenson <115705933+LyciaStenson@users.noreply.github.com>
2024-07-03 23:09:24 +0100

    Added 7.css, xp.css and 98.css

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,20 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<title>Lycia Stenson | Games Programmer</title>
+
+<script>
+    import "7.css/dist/7.scoped.css";
+    import "xp.css"
+    import "98.css"
+</script>
+
+<body>
+  <div class="window" style="margin: 32px; width: 250px">
+    <div class="title-bar">
+      <div class="title-bar-text">
+        My Projects
+      </div>
+    </div>
+    <div class="window-body">
+      <p>Hello, world!</p>
+    </div>
+  </div>
+</body>
