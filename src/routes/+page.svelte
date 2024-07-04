2b8fc31073cc91890a285d6025aa91140b93f2b1
Lycia Stenson <115705933+LyciaStenson@users.noreply.github.com>
2024-07-04 23:09:51 +0100

    Draggable windows from title bar working

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,13 @@
-<title>Lycia Stenson | Games Programmer</title>
+<script lang="ts">
+	import "7.css";
 
-<script>
-    import "7.css/dist/7.scoped.css";
-    import "xp.css"
-    import "98.css"
+	import Window from "./Window.svelte";
 </script>
 
-<body>
-  <div class="window" style="margin: 32px; width: 250px">
-    <div class="title-bar">
-      <div class="title-bar-text">
-        My Projects
-      </div>
-    </div>
-    <div class="window-body">
-      <p>Hello, world!</p>
-    </div>
-  </div>
+<title>Lycia Stenson | Games Programmer</title>
+
+<body class="background">
+	<img src="background.png" alt="Background" />
+	<Window />
+	<Window />
 </body>
