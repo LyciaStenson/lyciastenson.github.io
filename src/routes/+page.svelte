9f5418cdc34a9a4cf866a792629ba3ae7e99f11b
Lycia Stenson <115705933+LyciaStenson@users.noreply.github.com>
2025-05-05 19:43:57 +0100

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,7 +12,5 @@
 	<div class="flex flex-wrap justify-center gap-6">
 		<ProjectCard title="Nightbird" description="A custom game engine with OpenGL rendering." details="I started this project to develop my understanding of real-time graphics. Other students on my course joined the project, while my focus remains the graphics." link="https://nightbirdengine.github.io"></ProjectCard>
 		<ProjectCard title="Witch Treehouse Diorama" description="A shader diorama featuring Nightbird's shadow mapping and Blinn-Phong lighting model" details="I chose to take the opportunity to continue to develop Nightbird, while the assignment expected the use of an existing graphics engine." link="https://nightbirdengine.github.io"></ProjectCard>
-		<ProjectCard title="Graphics API Research" description="Why is the real-time graphics industry adopting low-level graphics APIs such as Vulkan over high-level APIs like OpenGL?" details="I am researching the adoption of low-level graphics APIs by the real-time graphics industry." link="https://lyciastenson.github.io"></ProjectCard>
-		<ProjectCard title="Sublime" description="A custom offline ray traced renderer. Currently developing a command-line interface." details="In future a Blender add-on will be developed to render scenes." link="https://lyciastenson.github.io"></ProjectCard>
 	</div>
 </div>
