42e71164ed9f19cd417f3b9a7494042b65bdd7e2
Lycia Stenson <115705933+LyciaStenson@users.noreply.github.com>
2025-09-15 10:20:31 +0100

    Added Dance Brawler Itch embed with dynamic dark mode

---
+++ src/lib/components/ItchEmbed.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import { onMount } from "svelte";
+	import { browser } from "$app/environment";
+	
+	export let gameName: string;
+	export let author: string;
+	export let gameId: string;
+
+	let isDark: boolean = false;
+
+	if (browser) {
+		onMount(() => {
+			const mediaQuery: MediaQueryList = window.matchMedia('(prefers-color-scheme: dark)');
+			isDark = mediaQuery.matches;
+			
+			const updateTheme = (e: MediaQueryListEvent) => {
+				isDark = e.matches;
+			};
+			
+			mediaQuery.addEventListener('change', updateTheme);
+			
+			return () => {
+				mediaQuery.removeEventListener('change', updateTheme);
+			};
+		});
+	}
+	
+	$: iframeSrc = `https://itch.io/embed/${gameId}${isDark ? '/?dark=true' : ''}`;
+</script>
+
+<iframe title="Itch Embed" height="167" frameborder="0" src={iframeSrc} width="552">
+</iframe>
--- src/routes/dance-brawler/+page.svelte
+++ src/routes/dance-brawler/+page.svelte
@@ -1,7 +1,13 @@
+<script lang="ts">
+	import ItchEmbed from "$lib/components/ItchEmbed.svelte";
+</script>
+
 <title>Dance Brawler | Lycia Stenson</title>
 
 <div class="pt-4 p-8">
 	<h1 class="pb-6 text-3xl font-bold">
 		Dance Brawler
 	</h1>
+	
+	<ItchEmbed gameName="dancebrawler" author="smallbagopeas" gameId="3577174" />
 </div>
