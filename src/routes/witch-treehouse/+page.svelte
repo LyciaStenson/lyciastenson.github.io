5850a696af96c3bc90c5f52170c36783679670f9
Lycia Stenson <115705933+LyciaStenson@users.noreply.github.com>
2025-09-24 18:10:35 +0100

    Added witch treehouse turnaround video

--- src/routes/witch-treehouse/+page.svelte
+++ src/routes/witch-treehouse/+page.svelte
@@ -4,4 +4,8 @@
 	<h1 class="text-3xl font-bold">
 		Witch Treehouse Diorama
 	</h1>
+
+	<video src="/videos/witch-treehouse/turnaround.mp4" autoplay muted loop controls width="800" class="rounded-xl shadow-lg">
+		<track kind="captions" />
+	</video>
 </div>
