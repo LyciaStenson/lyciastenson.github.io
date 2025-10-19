4f0af08c0d25212f795524d36c71720d80784325
Lycia Stenson <115705933+LyciaStenson@users.noreply.github.com>
2025-10-19 10:18:55 +0100

    Update +page.svelte

--- src/routes/3d-platformer/+page.svelte
+++ src/routes/3d-platformer/+page.svelte
@@ -5,12 +5,14 @@
 		3D Platformer
 	</h1>
 
-	<p class="text-lg">
-		A 3D platformer inspired by Super Mario 3D World, developed in Godot as a showcase of game feel for a university taster session.
-	</p>
-	<p class="text-lg">
-		The students could tweak parameters including acceleration, jump height, time to reach apex, time to fall back down, the presence of trailing cloud particles, to get a sense for how they affect the feel of the character.
-	</p>
+	<div class="space-y-3">
+		<p class="text-lg">
+			A 3D platformer inspired by Super Mario 3D World, developed in Godot as a showcase of game feel for a university taster session.
+		</p>
+		<p class="text-lg">
+			The students could tweak parameters including acceleration, jump height, time to reach apex, time to fall back down, and the presence of trailing cloud particles, to get a sense for how they affect the feel of the character.
+		</p>
+	</div>
 	
 	<video src="/3d-platformer/demo.mp4" autoplay muted loop playsinline controls width="600" class="rounded-xl shadow-lg">
 		<track kind="captions" />
