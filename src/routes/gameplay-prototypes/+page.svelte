3163285efb30030e236cdbb47e08edeaf9c87437
Lycia Stenson <115705933+LyciaStenson@users.noreply.github.com>
2025-10-20 17:18:54 +0100

    Update +page.svelte

--- src/routes/gameplay-prototypes/+page.svelte
+++ src/routes/gameplay-prototypes/+page.svelte
@@ -5,12 +5,25 @@
 		Godot Gameplay Prototypes
 	</h1>
 	
+	<p class="text-lg">
+		Throughout my research placement, I developed various gameplay prototypes in Godot. At the time, Godot 4 had recently released and I wanted to become more familiar with the engine.
+	</p>
+	
+	<h2 class="text-3xl font-bold">
+		3D Platformer
+	</h2>
+	
 	<div class="space-y-3">
 		<p class="text-lg">
 			A 3D platformer inspired by Super Mario 3D World, developed in Godot as a showcase of game feel for a university taster session.
 		</p>
+		
+		<video src="/3d-platformer/demo.mp4" autoplay muted loop playsinline controls width="600" class="rounded-xl shadow-lg">
+			<track kind="captions" />
+		</video>
+		
 		<p class="text-lg">
-			The students could tweak various parameters to get a sense for how they affect the feel of the character, including...
+			The students could tweak various parameters to get a sense for how they affect the feel of the character, and explore a simple platformer environment I build out of transformed rock models.
 		</p>
 		<div class="space-y-0">
 			<p class="text-lg">
@@ -33,12 +46,42 @@
 				<li>Presence of sound including running, jump, and double jump sound effects</li>
 			</ul>
 		</div>
-		<p>
-			I used <a class="font-bold" href="https://github.com/gdquest-demos/godot-4-3D-Characters">GDQuest's</a> Sophia character as she is Godot-themed and included movement animations.
+		<div class="space-y-0">
+			<p class="text-lg">
+				Asset Credits
+			</p>
+			<ul class="list-disc pl-8">
+				<li><a class="font-bold" href="https://github.com/gdquest-demos/godot-4-3D-Characters">GDQuest's</a> Godot-themed Sophia character with included movement animations</li>
+				<li><a class="font-bold" href="https://github.com/RPicster/godot4-demo-desert-light/tree/main">Raffaele Picca's</a> rock terrain models to build out the level</li>
+			</ul>
+		</div>
+	</div>
+	
+	<h2 class="text-3xl font-bold">
+		Doctor Who
+	</h2>
+	
+	<div class="space-y-3">
+		<p class="text-lg">
+			A 3D platformer inspired by Super Mario 3D World, developed in Godot as a showcase of game feel for a university taster session.
+		</p>
+		
+		<p class="text-lg">
+			The students could tweak various parameters to get a sense for how they affect the feel of the character, and explore a simple platformer environment I build out of transformed rock models.
 		</p>
 	</div>
 	
-	<video src="/3d-platformer/demo.mp4" autoplay muted loop playsinline controls width="600" class="rounded-xl shadow-lg">
-		<track kind="captions" />
-	</video>
+	<h2 class="text-3xl font-bold">
+		LIDAR
+	</h2>
+	
+	<div class="space-y-3">
+		<p class="text-lg">
+			The player controls a &apos;LIDAR scanner&apos; to place points on an invisible environment
+		</p>
+		
+		<p class="text-lg">
+			This project was developed as a
+		</p>
+	</div>
 </div>
