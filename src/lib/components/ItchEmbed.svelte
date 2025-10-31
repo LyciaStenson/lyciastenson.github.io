215c0ffb3f4dc7b2411b62f6667e1dc9e477c21a
Lycia Stenson <115705933+LyciaStenson@users.noreply.github.com>
2025-10-31 11:40:42 +0000

    Update ItchEmbed.svelte

--- src/lib/components/ItchEmbed.svelte
+++ src/lib/components/ItchEmbed.svelte
@@ -26,7 +26,7 @@
 	$: iframeSrc = `https://itch.io/embed/${gameId}${isDark ? '/?dark=true' : ''}`;
 </script>
 
-<div class="w-full max-w-[552px] aspect-552/167">
+<div class="w-full max-w-[552px] aspect-552/170 sm:aspect-552/167">
 	<iframe
 		title="Itch Embed"
 		class="w-full h-full border-0"
