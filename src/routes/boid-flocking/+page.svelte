95cc6673d8124f2c71d00690e00909a81355aa94
Lycia Stenson <115705933+LyciaStenson@users.noreply.github.com>
2025-11-11 11:02:35 +0000

    Update +page.svelte

--- src/routes/boid-flocking/+page.svelte
+++ src/routes/boid-flocking/+page.svelte
@@ -6,6 +6,6 @@
 	</h1>
 
 	<p class="text-lg">
-		I used C++ with Unreal Engine to develop the simulation. I utilised Unreal's Instanced Static Mesh Component to increase performance with hundreds of boids. Test.
+		I used C++ with Unreal Engine to develop the simulation. I utilised Unreal's Instanced Static Mesh Component to increase performance with hundreds of boids.
 	</p>
 </div>
