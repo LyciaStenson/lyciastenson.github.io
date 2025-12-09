46fe682ec36875622a28403c0511a8bbdc0c3419
Lycia Stenson <115705933+LyciaStenson@users.noreply.github.com>
2025-12-09 12:58:59 +0000

    Updating Nightbird page

--- src/routes/nightbird/+page.svelte
+++ src/routes/nightbird/+page.svelte
@@ -26,17 +26,49 @@
 		This project started as an OpenGL renderer to develop my understanding of graphics programming. The project developed into my own game engine. I migrated to Vulkan and further developed its capabilities such as the scene editor and project system.
 	</p>
 	
-	<div class="space-y-1">
-		<p class="text-lg">
-			The engine features runtime type reflection and a dynamic project system using RTTR.
-		</p>
-		<p class="text-lg">
-			The user's project code is compiled as a DLL which is loaded by the editor or game at runtime. Custom scene object classes are listed along with built-in types in the editor and can have custom functionality when added to the scene.
-		</p>
-		<p class="text-lg">
-			Scenes created in the editor can be saved and loaded in the game.
-		</p>
+	<img src="/nightbird/editor.png" width="600" alt="Portal issue" />
+	
+	<div class="space-y-2">
+		<h2 class="text-4xl font-bold">
+			Project System
+		</h2>
+		
+		<div class="space-y-1">
+			<p class="text-lg">
+				Nightbird uses a shared library project system. The user's project code including custom object types are compiled as a DLL which is then loaded at runtime by the editor or app. Custom scene objects are listed along with built-in types in the editor with custom functionality when added to the scene.
+			</p>
+			
+			<p class="text-lg">
+				This is implemented using RTTR &lpar;Run Time Type Reflection&rpar; using the library&apos;s macros for users to register their types.
+			</p>
+		</div>
 	</div>
 	
-	<img src="/nightbird/editor.png" width="600" alt="Portal issue" />
+	<div class="space-y-2">
+		<h2 class="text-4xl font-bold">
+			Scene
+		</h2>
+		
+		<div class="space-y-1">
+			<p class="text-lg">
+				Scenes created in the editor can be saved and loaded in the game.
+			</p>
+		</div>
+	</div>
+	
+	<div class="space-y-2">
+		<h2 class="text-4xl font-bold">
+			Vulkan Renderer
+		</h2>
+		
+		<div class="space-y-1">
+			<p class="text-lg">
+				Nightbird uses a forward renderer implemented with Vulkan supporting opaque and transparent materials.
+			</p>
+			
+			<p class="text-lg">
+				Planned features include deferred rendering and PBR based on the Disney Principled BRDF.
+			</p>
+		</div>
+	</div>
 </div>
